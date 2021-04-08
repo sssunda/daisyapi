9bfd95a74758af00df75aa95d9e19edfe74c2ec6
sssunda <daeun@wishket.com>
2021-04-08 22:09:15 +0900

    modify test

---
+++ tests/test_auth.py
@@ -0,0 +1,3 @@
+def test_auth(client):
+    resp = client.get("/auth")
+    assert resp.status_code == 200
--- tests/test_helloworld.py
+++
@@ -1,4 +0,0 @@
-def test_helloworld(client):
-    resp = client.get("/hello")
-    assert resp.status_code == 200
-    assert resp.data == b'Hello, World!'
